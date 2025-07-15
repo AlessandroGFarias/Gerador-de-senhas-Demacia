ca7a1e9deb75ff8f714cec5212a32f6cf7fe114a
Kauê Carlos Oliveira dos Santos <kauecarloslua@gmail.com>
2025-07-15 17:14:35 -0300

    Create main.py
    Backend do Gerador de senhas

---
+++ backend_gerador_senhas/main.py
@@ -0,0 +1,36 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+import random
+import string
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route("/gerar_senha", methods=["POST"])
+def gerar_senha():
+    data = request.json
+
+    tamanho = int(data.get("tamanho", 12))
+    usar_maiusculas = data.get("maiusculas", True)
+    usar_numeros = data.get("numeros", True)
+    usar_simbolos = data.get("simbolos", True)
+
+    caracteres = string.ascii_lowercase
+
+    if usar_maiusculas:
+        caracteres += string.ascii_uppercase
+    if usar_numeros:
+        caracteres += string.digits
+    if usar_simbolos:
+        caracteres += string.punctuation
+    if not caracteres:
+        return jsonify({"erro": "Nenhum tipo de caractere selecionado. "}), 400
+    
+    senha = ''.join(random.choice(caracteres) for _ in range(tamanho))
+
+    return jsonify({"senha": senha})
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+
